70a0bcccf6d3bf2c17e915fc8ba6b9f44e197435
Gleb Fokin <g.fokin@skyeng.ru>
2023-03-10 13:17:32 +0700

    Исправляет валидацию строки

--- pages/api/tasks/string.js
+++ pages/api/tasks/string.js
@@ -3,11 +3,11 @@ let string = "";
 export default function handler(req, res) {
   if (req.method === "POST") {
     try {
-      string = JSON.parse(req.body).string;
-
       if (typeof string !== "string") {
         return res.status(400).json({ error: "Передана не строка" });
       }
+
+      string = JSON.parse(req.body).string;
 
       return res.status(201).json({ string });
     } catch (error) {
