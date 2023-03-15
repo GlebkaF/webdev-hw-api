6e2312815f9374500d58c633ecff2bf3e4dc211a
Gleb Fokin <g.fokin@skyeng.ru>
2023-03-15 13:06:43 +0700

    Добавляет эндпоинт который иногда пятисотит

---
+++ pages/api/todos/with-error.js
@@ -0,0 +1,62 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+let lastId = 1;
+
+let todos = [
+  {
+    id: generateId(),
+    text: "Сделать чаю",
+  },
+  {
+    id: generateId(),
+    text: "Выпить чаю",
+  },
+  {
+    id: generateId(),
+    text: "Отдохнуть",
+  },
+];
+
+export default function handler(req, res) {
+  if (Math.random() > 0.5) {
+    return res
+      .status(500)
+      .json({ error: "Извините сервер упал, попробуйте позже" });
+  }
+
+  try {
+    if (req.method === "POST") {
+      try {
+        const todo = {
+          text: JSON.parse(req.body).text,
+          id: generateId(),
+        };
+
+        todos.push(todo);
+
+        return res.status(201).json({ todos: getTodos() });
+      } catch (error) {
+        console.error(error);
+
+        return res
+          .status(400)
+          .json({ error: "В теле запроса невалидный JSON" });
+      }
+    }
+    res.status(200).json({ todos: getTodos() });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+}
+
+export function setTodos(newTodos) {
+  todos = newTodos;
+}
+
+export function getTodos() {
+  return todos.slice(-10);
+}
+
+export function generateId() {
+  return lastId++;
+}
