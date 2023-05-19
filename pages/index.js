d934a825cab4447ec8d6e4bd9195183a7d58d4ff
Glebka Fokin <GlebkaF@users.noreply.github.com>
2023-05-19 10:56:34 +0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -3,5 +3,5 @@ import { Inter } from "next/font/google";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  return <>Нечего тут смотреть, проходите дальше</>;
+  return <>Нечего тут смотреть, на хероку, проходите дальше</>;
 }
